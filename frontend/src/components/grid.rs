ed62835e0e92747abb61ce64b8ffbb3f0b7a73e1
Jeff Mitchell <crusty.rustacean@gmail.com>
2026-02-01 15:41:25 -0800

    chore: re-factor to remove unwraps in Grid component

--- frontend/src/components/grid.rs
+++ frontend/src/components/grid.rs
@@ -3,7 +3,7 @@
 // dependencies
 use crate::domain::{Game, Player};
 use wasm_bindgen::JsCast;
-use web_sys::{HtmlCanvasElement, CanvasRenderingContext2d};
+use web_sys::{CanvasRenderingContext2d, HtmlCanvasElement};
 use yew::prelude::*;
 
 // grid component
@@ -14,17 +14,24 @@ pub fn Grid() -> Html {
     {
         let canvas_ref = canvas_ref.clone();
         use_effect(move || {
-            if let Some(canvas) = canvas_ref.cast::<HtmlCanvasElement>() {
-                let ctx = canvas
-                    .get_context("2d")
-                    .unwrap()
-                    .unwrap()
-                    .dyn_into::<CanvasRenderingContext2d>()
-                    .unwrap();
+            let Some(canvas) = canvas_ref.cast::<HtmlCanvasElement>() else {
+                log::error!("Failed to get a canvas element to draw the game board on.");
+                return;
+            };
 
-                
-                for i in 0 .. 8 {
-                    for j in 0 .. 8 {
+            let Ok(Some(context)) = canvas.get_context("2d") else {
+                log::error!("Failed to get 2d context.");
+                return;
+            };
+
+            let Ok(ctx) = context.dyn_into::<CanvasRenderingContext2d>() else {
+                log::error!("Failed to cast to CanvasRenderingContext2d");
+                return;
+            };
+
+            {
+                for i in 0..8 {
+                    for j in 0..8 {
                         let x = (i * 100) as f64;
                         let y = (j * 100) as f64;
                         if (i + j) % 2 == 0 {
@@ -55,13 +62,13 @@ pub fn Grid() -> Html {
                     let radius = 40.0;
 
                     ctx.begin_path();
-                    ctx.arc(center_x, center_y, radius, 0.0, 2.0 * std::f64::consts::PI).unwrap();
+                    let _ = ctx.arc(center_x, center_y, radius, 0.0, 2.0 * std::f64::consts::PI);
                     ctx.fill();
                 }
             }
         });
     }
-    
+
     html! {
         <canvas ref={canvas_ref} id="board" height=800 width=800 style="border: 1px solid black">
         </canvas>
