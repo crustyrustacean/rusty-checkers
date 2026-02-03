aadc9ad69c5cff5d53228818fc5cc89dae4ce708
Jeff Mitchell <crusty.rustacean@gmail.com>
2026-02-03 06:59:12 -0800

    improvement: add `captured_pieces` to state

--- frontend/src/state.rs
+++ frontend/src/state.rs
@@ -1,7 +1,7 @@
 // src/state.rs
 
 // dependencies
-use crate::domain::Game;
+use crate::domain::{Game, GamePiece};
 use yewdux::Store;
 
 #[derive(Default, Clone, PartialEq, Store)]
@@ -9,4 +9,5 @@ pub struct State {
     pub current_game: Game,
     pub selected_piece: Option<(usize, usize)>,
     pub valid_moves: Vec<(usize, usize)>,
+    pub captured_pieces: Vec<GamePiece>,
 }
